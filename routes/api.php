cc3cd23dfd0c31011c5fd05d5fa671ac5f380ffe
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2024-10-17 14:11:35 -0400

    api /info

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/info', function(Request $request) {
+    return response()->json([
+        'app' => env('APP_NAME'),
+        'version' => env('APP_VERSION', '1.0'),
+        'env' => env('APP_ENV', 'local'),
+        'url' => env('APP_URL', 'http://localhost'),
+    ]);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
