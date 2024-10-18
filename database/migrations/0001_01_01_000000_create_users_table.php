46a24d70c3f82478c125fd9a31f9124be1b363d9
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2024-10-18 10:25:19 -0400

    User access token

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             // $table->id();
-            $table->ulid('id');
+            $table->ulid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->boolean('enabled')->default(true);
             $table->rememberToken();
             $table->timestamps();
-            $table->primary('id');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
